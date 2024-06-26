3cfe85cff79b4822a72c1927c06ffd0e7e5e4bf0
SabirinMohamedAli <thaprinmohamett1333@gmail.com>
2024-06-26 21:12:55 +0300

    Update Products component

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -7,7 +7,7 @@ import product4 from '../images/11.jpg';
 
 const Products = () => {
   const products = [
-    { id: 1, name: 'Gabi Velvet Lip Combo', price: '$20.00', discount: '25%', image: product1 },
+    { id: 1, name: 'Gabi Velvet Lip Combo', price: '$30.00', discount: '25%', image: product1 },
     { id: 2, name: 'Sahin Beauty Blusher', price: '$17.00', discount: '30%', image: product2 },
     { id: 3, name: 'Kemsoff Concealer Stick', price: '$25.00', discount: '15%', image: product3 },
     { id: 4, name: 'Nurture Gold Serum', price: '$30.00', discount: '20%', image: product4 },
